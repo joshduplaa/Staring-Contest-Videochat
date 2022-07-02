e925d0d46e6af546bfe6a7143bbd7bee8246dc4b
Joshua Duplaa <joshd@joshD.localdomain>
2022-07-02 17:18:05 -0500

    finished server side code
    added comments

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+//Server file
 
-const io = require("socket.io")(server, {
+const app = require("express")();  //Express App framework        
+const server = require("http").createServer(app);
+
+//enable cross origin requests with Cors "middleware"
+const cors = require("cors");   
+
+//Socket.io server-side instance
+const io = require("socket.io")(server, {            
     cors:{
-        origin: "*",
+        origin: "*",    //allows access from all origins
         methods: ["GET", "POST"]
     }
 });
 
 app.use(cors());
 
+//Declare port 5000
 const PORT = process.env.PORT || 5000;
 
+//root route
 app.get("/", (req, res) => {
     res.send('server is running');
 });
 
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+//back end socket code.
+//Sockets are used for realtime data transmission
+io.on('connection', (socket) =>{
+    //call back function that provides connection socket //a call back fucntion is a function passed as an argument for another function
+    
+    //gives me an ID on the frontend
+    socket.emit('me', socket.id);
+
+    //disconnect sockethandler displays end call message
+    socket.on('disconnect', () =>{
+        socket.broadcast.emit("callended")
+    });
+
+    //call user socket handler. 
+    socket.on("calluser", ({userToCall, signalData, from, name}) =>{
+        io.to(userToCall).emit("calluser", {signal: signalData, from, name});
+    });
+
+    socket.on("answercall", (data) =>{
+            io.to(data.to).emit("callaccepted", data.signal);
+    });
+});
